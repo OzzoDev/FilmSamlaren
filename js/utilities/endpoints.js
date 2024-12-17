be15a50a3028e5ed7963073f3e99867c5459a315
oscar <oscar.burman005@gmail.com>
2024-12-17 12:39:58 +0100

    Created endpoints.js to store all endpoints

---
+++ js/utilities/endpoints.js
@@ -0,0 +1,7 @@
+/*File to store all api endpoints*/
+
+export const IMBD_URL = "https://imdb236.p.rapidapi.com/imdb";
+
+export const IMBD_ENDPOINTS = {
+  topMovies: "https://imdb236.p.rapidapi.com/imdb/top250-movies",
+};
